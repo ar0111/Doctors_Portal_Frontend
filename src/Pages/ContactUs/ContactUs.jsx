930aaba3fd8a023ba5ec5fcec4e0b59b645c8006
Arafat-Rahman <arafat.cuet99@gmail.com>
2024-02-18 13:37:31 +0600

    Minor Update

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -12,6 +12,7 @@ const ContactUs = () => {
         emailjs.sendForm('service_lf2tmgd', 'template_cosj09m', form.current, 'tJL8oiV37682Rs2bq')
           .then((result) => {
               console.log(result.text);
+              toast.success("Thank you for reaching out to us. We will get back to you soon")
               e.target.reset();
           }, (error) => {
               console.log(error.text);
@@ -34,7 +35,7 @@ const ContactUs = () => {
                         <div className="sm:col-span-3 my-3">
                             <label className="text-start block text-sm font-medium leading-6 text-gray-900">Email</label>
                             <div className="mt-2">
-                                <input type="email" name="user_email" id="first-name" autoComplete="given-name" className="p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                <input type="email" name="email_id" id="email_id" autoComplete="given-name" className="p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                             </div>
                         </div>
 
